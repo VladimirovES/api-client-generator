c3afd813bb35a83de85cb08d45d0439874feaeb7
evgeniy_vladimirov <vladimiroves.qa@gmail.com>
2025-05-28 17:57:03 +0300

    update loger

--- src/my_codegen/utils/logger.py
+++ src/my_codegen/utils/logger.py
@@ -26,6 +26,9 @@ def configure_logging():
     ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(logging.INFO)
 
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter) 
+
     logger.addHandler(ch)
 
 
